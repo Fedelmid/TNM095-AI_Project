127b39834c70f090c30e6da9b6af8d726a72ebbb
Anneli Nilsson <blueprint.ace@gmail.com>
2020-09-21 13:57:00 +0200

    Smoother cohesion

---
+++ AI_Game_Mechanic/Assets/Scripts/BehaviourScripts/SteeredCohesionBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behaviour/Steered Cohesion")]
+public class SteeredCohesionBehaviour : FlockBehaviour // smoother type of cohesion, no flickering
+{
+
+    Vector3 currentVelocity;
+    public float agentSmoothTime = 0.5f; // 0.5 sec kinda, dependent on frame rate
+
+
+    // find middle point between all neighbours and try to move there
+    public override Vector3 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+        // if no neighbours, return no adjustment
+        if (context.Count == 0)
+            return Vector3.zero;
+
+        // add all points together and average
+        Vector3 cohesionMove = Vector3.zero;
+        foreach (Transform item in context)
+        {
+            cohesionMove += item.position;
+        }
+        cohesionMove /= context.Count;
+
+        // create offset from agent position
+        cohesionMove -= agent.transform.position;
+        cohesionMove = Vector3.SmoothDamp(agent.transform.forward, cohesionMove, ref currentVelocity, agentSmoothTime);
+        return cohesionMove;
+    }
+}
