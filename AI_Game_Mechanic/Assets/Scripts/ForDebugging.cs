0d8ec6ef49aeb013ec4f54e39d97896340bd8a9a
Anneli N <blueprint.ace@gmail.com>
2020-10-04 15:01:22 +0200

    Modified materials and debug, added cinemachine package

--- AI_Game_Mechanic/Assets/Scripts/ForDebugging.cs
+++ AI_Game_Mechanic/Assets/Scripts/ForDebugging.cs
@@ -13,6 +13,7 @@ public class ForDebugging : MonoBehaviour
         GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
         obj.transform.localScale = boxed.boxLimit;
         obj.transform.position = boxed.center;
+        obj.GetComponent<BoxCollider>().enabled = false;
 
         obj.GetComponent<Renderer>().material = c;
     }
