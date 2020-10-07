4f73189893e49cbee44bc525cf2d12cabdd9171b
Anneli N <blueprint.ace@gmail.com>
2020-10-07 18:35:40 +0200

    Changed the look of the swarm area

--- AI_Game_Mechanic/Assets/Scripts/SwarmArea.cs
+++ AI_Game_Mechanic/Assets/Scripts/SwarmArea.cs
@@ -7,10 +7,17 @@ public class SwarmArea : MonoBehaviour
 
     public StayInBoxBehaviour boxed;
     public GameObject swarmArea;
+    public GameObject swarmAreaColored;
 
     void Start()
     {
         swarmArea.transform.localScale = boxed.boxLimit;
-        swarmArea.transform.position = boxed.center;
+        swarmArea.transform.position = boxed.center; 
+
+        swarmAreaColored.transform.localScale = boxed.boxLimit;
+        swarmAreaColored.transform.position = new Vector3(boxed.center.x, -1.95f, boxed.center.z);
+
+
+
     }
 }
