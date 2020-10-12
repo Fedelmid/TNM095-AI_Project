86a68bd6b9ea53f15b64f2bd64df1f593f317974
Anneli N <blueprint.ace@gmail.com>
2020-10-12 13:42:35 +0200

    Added flock patrol behaviour

---
+++ AI_Game_Mechanic/Assets/Scripts/FlockPatrol.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockPatrol : MonoBehaviour
+{
+    public StayInRadiusBehaviour target;
+    public float changeTargetAfter = 3f;
+
+    public Transform pointA, pointB;
+
+    bool isTraveling = false;
+
+    void Start()
+    {
+        
+        target.center = pointA.position;
+        Debug.Log("Start target "+target.center);
+        //StartCoroutine("ChangeTarget");
+    }
+
+    void Update()
+    {
+        if(!isTraveling)
+            StartCoroutine("ChangeTarget");
+    }
+
+    private IEnumerator ChangeTarget()
+    {
+        isTraveling = true;
+        target.center = (target.center == pointA.position) ? pointB.position : pointA.position;
+        Debug.Log("Changed target " + target.center);
+        yield return new WaitForSeconds(changeTargetAfter);
+        isTraveling = false;
+    }
+}
