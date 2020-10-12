62d1ff2d3ebd824e946515b67586bd51a7ea9e77
Anneli N <blueprint.ace@gmail.com>
2020-10-12 09:45:42 +0200

    Flock hover object behaviour

---
+++ AI_Game_Mechanic/Assets/Scripts/FlockHoverObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockHoverObject : MonoBehaviour
+{
+    public StayInRadiusBehaviour point;
+
+    void Start()
+    {
+        point.center = transform.GetChild(0).transform.position;
+    }
+}
